d6bb5a8ddbed9d1b5143dac7fb84372cbbc169a2
galym2000gt@gmail.com <galym2000gt@gmail.com>
2019-04-14 15:33:40 +0600

    Программа вычисляет цену товара на необходимый вес

--- Gcalculator/Form1.Designer.cs
+++ Gcalculator/Form1.Designer.cs
@@ -35,6 +35,11 @@
             this.label2 = new System.Windows.Forms.Label();
             this.button_WOC2 = new ePOSOne.btnProduct.Button_WOC();
             this.label3 = new System.Windows.Forms.Label();
+            this.button_WOC3 = new ePOSOne.btnProduct.Button_WOC();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -42,11 +47,11 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Cooper Black", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(33, 52);
+            this.label1.Location = new System.Drawing.Point(-3, 109);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(198, 34);
+            this.label1.Size = new System.Drawing.Size(296, 34);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Введите цену";
+            this.label1.Text = "Введите цену за 1шт";
             // 
             // button_WOC1
             // 
@@ -60,7 +65,7 @@
             this.button_WOC1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.button_WOC1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_WOC1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button_WOC1.Location = new System.Drawing.Point(691, 12);
+            this.button_WOC1.Location = new System.Drawing.Point(838, 12);
             this.button_WOC1.Name = "button_WOC1";
             this.button_WOC1.OnHoverBorderColor = System.Drawing.Color.White;
             this.button_WOC1.OnHoverButtonColor = System.Drawing.Color.White;
@@ -81,11 +86,11 @@
             this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox1.ForeColor = System.Drawing.Color.White;
-            this.textBox1.Location = new System.Drawing.Point(266, 60);
+            this.textBox1.Location = new System.Drawing.Point(405, 57);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(264, 29);
             this.textBox1.TabIndex = 2;
-            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress_1);
             // 
             // textBox2
             // 
@@ -95,22 +100,22 @@
             this.textBox2.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox2.ForeColor = System.Drawing.Color.White;
-            this.textBox2.Location = new System.Drawing.Point(266, 115);
+            this.textBox2.Location = new System.Drawing.Point(405, 112);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(264, 29);
             this.textBox2.TabIndex = 2;
-            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress_1);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Cooper Black", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(33, 109);
+            this.label2.Location = new System.Drawing.Point(-3, 54);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(180, 34);
+            this.label2.Size = new System.Drawing.Size(260, 34);
             this.label2.TabIndex = 0;
-            this.label2.Text = "Введите вес";
+            this.label2.Text = "Введите название";
             // 
             // button_WOC2
             // 
@@ -124,7 +129,7 @@
             this.button_WOC2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.button_WOC2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_WOC2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button_WOC2.Location = new System.Drawing.Point(555, 109);
+            this.button_WOC2.Location = new System.Drawing.Point(691, 212);
             this.button_WOC2.Name = "button_WOC2";
             this.button_WOC2.OnHoverBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.button_WOC2.OnHoverButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
@@ -142,10 +147,86 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(260, 207);
+            this.label3.Location = new System.Drawing.Point(401, 274);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(0, 36);
             this.label3.TabIndex = 0;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button_WOC3
+            // 
+            this.button_WOC3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button_WOC3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_WOC3.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.button_WOC3.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button_WOC3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button_WOC3.FlatAppearance.BorderSize = 0;
+            this.button_WOC3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button_WOC3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button_WOC3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_WOC3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_WOC3.Location = new System.Drawing.Point(804, 12);
+            this.button_WOC3.Name = "button_WOC3";
+            this.button_WOC3.OnHoverBorderColor = System.Drawing.Color.White;
+            this.button_WOC3.OnHoverButtonColor = System.Drawing.Color.White;
+            this.button_WOC3.OnHoverTextColor = System.Drawing.Color.Navy;
+            this.button_WOC3.Size = new System.Drawing.Size(28, 28);
+            this.button_WOC3.TabIndex = 1;
+            this.button_WOC3.Text = "_";
+            this.button_WOC3.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.button_WOC3.TextColor = System.Drawing.Color.White;
+            this.button_WOC3.UseVisualStyleBackColor = false;
+            this.button_WOC3.Click += new System.EventHandler(this.button_WOC3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.textBox3.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.ForeColor = System.Drawing.Color.White;
+            this.textBox3.Location = new System.Drawing.Point(405, 167);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(264, 29);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cooper Black", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(0, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(285, 34);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Введите вес за 1 шт";
+            // 
+            // textBox4
+            // 
+            this.textBox4.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.textBox4.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox4.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.ForeColor = System.Drawing.Color.White;
+            this.textBox4.Location = new System.Drawing.Point(405, 217);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(264, 29);
+            this.textBox4.TabIndex = 2;
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cooper Black", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(-3, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(381, 34);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Введите необходимый вес ";
             // 
             // Form1
             // 
@@ -153,12 +234,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(733, 393);
+            this.ClientSize = new System.Drawing.Size(880, 425);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button_WOC2);
+            this.Controls.Add(this.button_WOC3);
             this.Controls.Add(this.button_WOC1);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -180,6 +266,11 @@
         private System.Windows.Forms.Label label2;
         private ePOSOne.btnProduct.Button_WOC button_WOC2;
         private System.Windows.Forms.Label label3;
+        private ePOSOne.btnProduct.Button_WOC button_WOC3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Gcalculator/Form1.cs
+++ Gcalculator/Form1.cs
@@ -3,11 +3,14 @@ using System.Windows.Forms;
 
 namespace Gcalculator
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form 
     {
-        double weight;
+        double weightone;
         double price;
+        double weight;
         double answer;
+        string name;
+
 
 
         public Form1()
@@ -21,35 +24,142 @@ namespace Gcalculator
         }
 
 
-        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (e.KeyChar == (char)Keys.Enter)
-            {
-                textBox2.Focus();
-            }
-        }
-
         private void Form1_Activated(object sender, EventArgs e)
         {
             textBox1.Focus();
         }
 
-        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+
+        private void button_WOC2_Click(object sender, EventArgs e)
         {
+            if ((textBox1.Text == "") || (textBox4.Text == ""))
+            {
+                label3.Text = "Не все данные введны";
+            }
+            else
+            {
+                name = textBox1.Text;
+                price = double.Parse(textBox2.Text);
+                weightone = double.Parse(textBox3.Text);
+                weight = double.Parse(textBox4.Text);
+                answer = price * (weight / weightone);
+
+                label3.Text = "Итог: " + answer.ToString("n") + "тг" + "\nза " + weight.ToString() + "кг";
+
+
+                textBox1.Focus();
+                textBox1.Text = "";
+            }
+        }
+
+        public static void function_textbox(TextBox text, KeyPressEventArgs e, char a, char b)
+        {
+
+            if ((e.KeyChar >= a) && (e.KeyChar <= b))
+            {
+                return;
+            }
+
+            if (e.KeyChar == '.')
+            {
+                e.KeyChar = ',';
+            }
+
+            if (e.KeyChar == ',')
+            {
+                if (text.Text.IndexOf(',') != 1)
+                {
+                    e.Handled = true;
+                }
+                return;
+            }
+
+            if (char.IsControl(e.KeyChar))
+            {
+                return;
+            }
+            e.Handled = true;
+
+        }
+
+
+        private void button_WOC3_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+
+        private void textBox1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            function_textbox(textBox1, e, 'а', 'я');
+
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                textBox2.Focus();
+            }
+
+            if ((e.KeyChar == (char)Keys.Back) && (textBox3.Text == ""))
+            {
+                textBox1.Focus();
+            }
+        }
+
+
+
+        private void textBox2_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            function_textbox(textBox2, e, '0', '9');
+
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                textBox3.Enabled = true;
+                textBox3.Focus();
+            }
+
+            if ((e.KeyChar == (char)Keys.Back) && (textBox3.Text == ""))
+            {
+                textBox3.Focus();
+            }
+        }
+
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            function_textbox(textBox4, e, '0', '9');
+
             if (e.KeyChar == (char)Keys.Enter)
             {
                 button_WOC2.Focus();
             }
+
+            if ((e.KeyChar == (char)Keys.Back) && (textBox2.Text == ""))
+            {
+                if (textBox3.Enabled == false)
+                {
+                    textBox3.Focus();
+                }
+                else
+                {
+                    textBox3.Focus();
+                }
+            }
         }
 
-        private void button_WOC2_Click(object sender, EventArgs e)
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
         {
-            price = Convert.ToDouble(textBox1.Text);
-            weight = Convert.ToDouble(textBox2.Text);
+            function_textbox(textBox3, e, '0', '9');
 
-            answer = price * weight;
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                textBox4.Focus();
+                textBox4.Text = "";
+            }
 
-            label3.Text = "Итог: " + answer.ToString("n") + "тг" + "\nза " + weight.ToString() + "кг";
+            if ((e.KeyChar == (char)Keys.Back) && (textBox3.Text == ""))
+            {
+                textBox2.Focus();
+            }
         }
     }
 }
